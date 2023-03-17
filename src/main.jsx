61178ca2f765fa7a3208d43160175b1f3353b022
Rickazuo <rick3209@gmail.com>
2023-03-17 19:20:59 -0300

    add footer

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Footer from "./components/footer";
 import "./index.css";
 import LoginPage from "./pages/loginPage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <LoginPage />
+    <Footer />
   </React.StrictMode>
 );
--- src/pages/loginPage.jsx
+++ src/pages/loginPage.jsx
@@ -4,51 +4,53 @@ import vertigo from "../assets/logo.svg";
 function loginPage() {
   return (
     <div className={style.container}>
-      <aside className={style.leftBar}>
-        <div>
-          <img className={style.logo} src={vertigo} alt="logo of company" />
-          <h1 className={style.title}>Acesse a plataforma</h1>
-          <p className={style.subtitle}>
-            Faça login ou registre-se para começar a construir seus projetos
-            ainda hoje.
-          </p>
-          <div className={style.inputs}>
-            <div className={style.email}>
-              <label htmlFor="email">E-mail</label>
-              <input
-                type="email"
-                id="email"
-                placeholder="Digite seu e-mail"
-                required
-              />
-            </div>
-            <div className={style.password}>
-              <div className={style.labelPassword}>
-                <label htmlFor="password">Senha</label>
-                <span>
-                  <a href="#">Esqueceu a senha?</a>
-                </span>
-              </div>
-              <input
-                type="password"
-                id="password"
-                placeholder="Digite sua senha"
-                required
-              />
-            </div>
-            <button className={style.buttonEnter} type="submit">
-              Entrar
-            </button>
-            <p className={style.textFooter}>
-              Ainda não tem uma conta?{" "}
-              <span>
-                <a href="#"> Inscreva-se</a>
-              </span>
+      <div className={style.main}>
+        <aside className={style.leftBar}>
+          <div>
+            <img className={style.logo} src={vertigo} alt="logo of company" />
+            <h1 className={style.title}>Acesse a plataforma</h1>
+            <p className={style.subtitle}>
+              Faça login ou registre-se para começar a construir seus projetos
+              ainda hoje.
             </p>
+            <div className={style.inputs}>
+              <div className={style.email}>
+                <label htmlFor="email">E-mail</label>
+                <input
+                  type="email"
+                  id="email"
+                  placeholder="Digite seu e-mail"
+                  required
+                />
+              </div>
+              <div className={style.password}>
+                <div className={style.labelPassword}>
+                  <label htmlFor="password">Senha</label>
+                  <span>
+                    <a href="#">Esqueceu a senha?</a>
+                  </span>
+                </div>
+                <input
+                  type="password"
+                  id="password"
+                  placeholder="Digite sua senha"
+                  required
+                />
+              </div>
+              <button className={style.buttonEnter} type="submit">
+                Entrar
+              </button>
+              <p className={style.textFooter}>
+                Ainda não tem uma conta?{" "}
+                <span>
+                  <a href="#"> Inscreva-se</a>
+                </span>
+              </p>
+            </div>
           </div>
-        </div>
-      </aside>
-      <div className={style.rightBackground}></div>
+        </aside>
+        <div className={style.rightBackground}></div>
+      </div>
     </div>
   );
 }
